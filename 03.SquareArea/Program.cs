ff11032dfde5d0a8b166cf2d9c5bc370519da746
Andarz <arzjaev@gmail.com>
2023-09-12 18:23:24 +0300

    Add project files.

---
+++ 03.SquareArea/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _03.SquareArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int side = int.Parse(Console.ReadLine());
+            Console.WriteLine(side * side);
+        }
+    }
+}
